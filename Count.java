4def171f5c8b7270cecffa54c5de52245f7ee234
Dalal Alshehri <Dalal Alshehri>
2016-02-04 11:34:05 -0600

    Good job, I made it

--- Count.java
+++ Count.java
@@ -3,6 +3,16 @@ public class Count
 {
 public static void main (String [] args)
     {
-System.out.println("Hello there!");
+	  int count = 0;
+      for (int i=1;i<=1000000; i++)
+        {  
+    	 if ((i%9==0 && i%13==0 && i%27==0) || i%81==0)
+    		 {
+    		 count++;
+    		 }
+         }
+	   
+      System.out.println("The number of numbers between 1 and 1 million that are evenly divisible by 9, 13, 27, or 81 is ");
+      System.out.println(count);
     }
 }
