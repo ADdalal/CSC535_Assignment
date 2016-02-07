12b40312538c6849e0b780f72db47af6dd28f5cc
Dalal Alshehri <Dalal Alshehri>
2016-02-07 16:57:19 -0600

    I edited it

--- Count.java
+++ Count.java
@@ -4,9 +4,13 @@ public class Count
 public static void main (String [] args)
     {
 	  int count = 0;
+	  //this declaration for count the numbers
+	  
       for (int i=1;i<=1000000; i++)
+      //this loop used to go from 1 to one million
         {  
-    	 if ((i%9==0 && i%13==0 && i%27==0) || i%81==0)
+    	 if ((i%9==0 || i%13==0 || i%27==0) || i%81==0)
+    	 //this condition for deciding which number is evenly divisible by 9,13,27, or 81
     		 {
     		 count++;
     		 }
